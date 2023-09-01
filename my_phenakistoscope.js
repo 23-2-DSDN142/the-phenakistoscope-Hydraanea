8a9a450cff4d4732fb5d5e60c3063a896a055eda
Hydraanea <132314937+Hydraanea@users.noreply.github.com>
2023-09-02 00:06:32 +1200

    fish

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -7,16 +7,17 @@ function setup_pScope(pScope){
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
   pScope.load_image("stingray" , "png");
+  pScope.load_image("fish" , "png");
 
 }
 
 function setup_layers(pScope){
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  new PLayer(null, "#90D1D3");  //lets us draw the whole circle background, ignoring the boundaries
 
   var layer1 = new PLayer(stingray);
-  layer1.mode( SWIRL(4) );
-  layer1.set_boundary( 200, 1500 );
+  layer1.mode( SWIRL(5) );
+  layer1.set_boundary( 100, 1500 );
 
   var layer2 = new PLayer(squares);
   layer2.mode( RING );
@@ -25,21 +26,28 @@ function setup_layers(pScope){
 }
   
 
-function stingray(x, y, animation, pScope){
+function stingray(stingrayx, y, animation, pScope){
   scale(0.5);
-  var stingrayx = animation.wave(5)*100;
+  var stingrayx = animation.wave(3)*400;
   pScope.draw_image("stingray",stingrayx,y); 
 
 
-  // scale(animation.frame*2);
-
-  // fill (0,100,200)
-  // ellipse(0,0,30,200); // draw head
- 
-  
-
+scale(0.5);
+var fishx = animation.wave(1)*100;
+pScope.draw_image("fish",fishx,y); 
 
 }
+
+
+// function fish(fishx, y, animation, pScope){
+//   // scale(0);
+//   var fishx = animation.wave(2)*200;
+//   pScope.draw_image("fish",fishx,y); 
+// // pop()
+
+// }
+
+
 
 function squares(x, y, animation, pScope){
 
@@ -49,10 +57,10 @@ function squares(x, y, animation, pScope){
   let backgroundArcStart = 270 - angleOffset;
   let backgroundArcEnd = 270 + angleOffset;
 
-  fill(100, 400, 0) //INNER CIRCLE BACKGRPUND)
+  fill("#50723C") //INNER CIRCLE BACKGRPUND)
   arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
 
-  fill(200,200,0) // INNER CIRCLE IMAGE
+  fill("#72B01D") // INNER CIRCLE IMAGE
   rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
 
 }
